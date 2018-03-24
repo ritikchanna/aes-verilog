6ee89d575d2d3ce85588dd43ae3a5c059fe97106
Ritik Channa <chnritik@gmail.com>
2018-03-24 10:20:03 +0530

    Integrated all functions for Encrypt Process

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/superuser/Workspace/Verilog/AES/AES/TestBench_isim_beh.exe" -prj "/home/superuser/Workspace/Verilog/AES/AES/TestBench_beh.prj" "work.TestBench" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/home/superuser/Workspace/Verilog/AES/AES/TestBench_isim_beh.exe" -prj "/home/superuser/Workspace/Verilog/AES/AES/TestBench_beh.prj" "work.TestBench" "work.glbl" 
