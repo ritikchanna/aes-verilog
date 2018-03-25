f147de4c9a59fe5b1867bb7d4f1fb2b7c2a3e41f
anubhawdwd <36097141+anubhawdwd@users.noreply.github.com>
2018-03-25 14:20:41 +0530

    Revert "Integrated all functions for Encrypt Process"
    This reverts commit af5fb5aa103591cd9a9abc0c88dcec07654d1611.

--- Encrypt.v
+++ Encrypt.v
@@ -1,14 +1,13 @@
 `timescale 1ns / 1ps
 module Encrypt(input wire [127:0] Block,
 					input wire [127:0] Key,
-					input wire next,
-					output reg ready,
+					input wire clk,
+					input wire reset,
 					output reg [127:0] Result
 					);
 
 
 reg [7 : 0] sbox [0 : 255];
-
 integer i;
 //Adds current RoundKey to the state					
 function [127:0] AddRoundKey(input [127:0] State,input [127:0] RoundKey);
@@ -395,7 +394,7 @@ localparam FINAL_UPDATE = 3'h3;
 //TODO change according to logic
 reg [2 : 0]  update_type = INIT_UPDATE;
 reg [2 : 0] counter;
-
+reg ready = 1'h1;
 
  //----------------------------------------------------------------
   // round_logic
@@ -420,28 +419,22 @@ reg [2 : 0] counter;
 				block_new = AddRoundKey(block_old,key_old);
 				//TODO key new from routine
 				key_new = Key;
-				counter = 3'h1;
+				counter = 3'h0;
 				update_type = MAIN_UPDATE;
-				ready = 1'h1;				
+				ready = 1'h1;
           end
         MAIN_UPDATE:
           begin
-			  $display("MAIN_UPDATE %h",counter);
-			  ready = 1'h0;
-			
-			
-			 temp <= SubBytes(block_new);
-			 temp <= ShiftRow(temp);
-			 temp <= mixcolumn(temp);
+			  $display("MAIN_UPDATE");
+			 temp = SubBytes(block_new);
+			 temp = ShiftRow(temp);
+			 temp = mixcolumn(temp);
 			 //TODO addroundkey
-          block_old <= block_new;
-			 block_new <= temp;
+          block_old = block_new;
+			 block_new = temp;
 			 //todo increment counter
           //if(counter >= 11)
-			 counter <= counter +1;
-			 if(counter == 3'h3)
-			update_type <= FINAL_UPDATE;
-			ready = 1'h1;
+			update_type = FINAL_UPDATE;
 			 end
 
         FINAL_UPDATE:
@@ -455,14 +448,13 @@ reg [2 : 0] counter;
 			 block_new = temp;
 			 Result = block_new;
 			 update_type = NO_UPDATE;
-			ready = 1'h1;
+			 ready = 1'h1;
           end
 
         default:
           begin
           end
       endcase // case (update_type)
-		
     end // round_logic
 
 
--- TestBench.v
+++ TestBench.v
@@ -1,16 +1,18 @@
-	`timescale 1ns / 1ps
-	module TestBench;
-	reg [127:0] TestState;
-	reg [127:0] TestKey;
-	wire [127:0] TestResult;
-	reg next = 1'b1;
-	reg reset;
-	wire ready;
-	//TODO connect using name and connect other parameters too !!
-	Encrypt e_module(TestState,TestKey,next,ready,TestResult);
-	initial
-	begin
-	TestState=128'D1;
-	TestKey=128'D8;
-	end
-	endmodule
+`timescale 1ns / 1ps
+module TestBench;
+reg [127:0] TestState;
+reg [127:0] TestKey;
+wire [127:0] TestResult;
+reg clk;
+reg reset;
+//TODO connect using name and connect other parameters too !!
+Encrypt e_module(TestState,TestKey,,,TestResult);
+initial
+begin
+TestState=128'D1;
+TestKey=128'D8;
+
+
+
+end
+endmodule
